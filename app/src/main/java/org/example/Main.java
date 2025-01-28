ee99cea7670c900554fe9e34fed41ec23a348ba1
hahafunnyuserna <madeforschool69@gmail.com>
2025-01-27 21:49:33 -0600

    final

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -1,19 +1,21 @@
 package org.example;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class App {
+public class Main {
     public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-
+    {   Scanner scan = new Scanner(System.in);
+      
+        int input = 0;
+        
+   
         System.out.println("Please enter any number.");
-        int input = scan.nextInt();
+        input = scan.nextInt();
 
         Factortron factor = new Factortron();
         ArrayList<Integer> finalList = factor.primeFactors(input);
         System.out.println("Prime factors of " + input + ":\n" + finalList);
-
 
         scan.close();
     }
