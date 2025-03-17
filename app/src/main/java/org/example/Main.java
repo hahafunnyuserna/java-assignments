317935214c465eec74e2c31fc7ed305db2398851
hahafunnyuserna <madeforschool69@gmail.com>
2025-03-17 11:49:16 -0500

    incomplete (?)
    unable to test, cannot read input file

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -1,33 +1,58 @@
 package org.example;
 
+import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-    ListOfExecutions list = new ListOfExecutions();
-   
-    list.listAdd("Buy milk");
-    list.listAdd("Buy eggs");
-    list.listAdd("Prepare a lesson for CSC 122");
-    list.listAdd("Sow beet seeds");
-   
-    list.listComplete("Buy eggs");
-   
-    // Pretty prints a list of each of the four tasks above
-    list.printAll();
-   
-    // Pretty prints the task "Buy eggs"
-    list.printComplete();
-   
-    // Pretty prints the tasks "Buy milk",
-    // "Prepare a lesson for CSC 122", and "Sow beet seeds"
-    list.printIncomplete();
-   
-    // Clears the to-do list
-    list.listClear();
-   
-    // Pretty prints a message saying that the list is empty
-    list.printAll();
-    
+public class Main {
+
+    public static void main(String... args) {
+        String input = "cheese_data.csv";
+        String output = "output.txt";
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("cheese_data.csv"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
+
+            String line = br.readLine();
+
+            while (line != null) {
+
+                String[] cheeses = line.split(",");
+
+                for (String cheese : cheeses)
+                {
+                    bw.write(cheese);
+                    bw.write(", ");
+                }
+                bw.newLine();
+
+                String id = cheeses[0];
+                String manufCode = cheeses[1];
+                String manufType = cheeses[2];
+                double moisture = Double.parseDouble(cheeses[3]);
+                String flavorProfile = cheeses[4];
+                String traits = cheeses[5];
+                boolean organic = Boolean.valueOf(cheeses[6]);
+                String cheeseType = cheeses[7];
+                String milkType = cheeses[8];
+                String milkTreatment = cheeses[9];
+                String rindType = cheeses[10];
+                String cheeseName = cheeses[11];
+                String fatLevel = cheeses[12];
+                
+               
+            }
+
+            br.close();
+            
+        } catch (FileNotFoundException fe) {
+            System.out.println("file : " + input + " not found");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        
     }
-  }
+
+}
