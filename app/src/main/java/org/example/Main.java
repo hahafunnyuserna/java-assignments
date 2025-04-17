979c42a044f516b7baf2126ee7338c2ddb6068b6
hahafunnyuserna <madeforschool69@gmail.com>
2025-04-17 15:23:56 -0500

    final

---
+++ app/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> nums1 = List.of(1, 3, 5, 7);
+        List<Integer> nums2 = List.of(2, 4, 6, 8);
+        List<Integer> mergedNumbers = ListMerger.MergeLists(nums1, nums2);
+        System.out.println(mergedNumbers);
+       
+        List<String> colors1 = List.of("Red", "Green", "Blue");
+        List<String> colors2 = List.of("White", "Black", "Orange", "Pink", "Fuschia");
+        List<String> mergedWords = ListMerger.MergeLists(colors1, colors2);
+        System.out.println(mergedWords);
+      }
+}
